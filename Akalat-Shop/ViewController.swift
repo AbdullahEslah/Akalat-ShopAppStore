5ac81701fa095add545662ca5afc83f0e944b3d5
Abdallah Eslah <abdallaheslah97@gmail.com>
2021-12-16 05:34:02 +0200

    Initial Commit

---
+++ Akalat-Shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Akalat-Shop
+//
+//  Created by Macbook on 16/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
